0ca8b5f8ee680c28695788a25cd63f6b948cf1be
Richard Hagen <richard.hagen@gmail.com>
2020-10-23 19:24:59 +0200

    cleanup

--- README.md
+++ README.md
@@ -9,34 +9,3 @@ npm run dev
 # or
 yarn dev
 ```
-
-
-
-Helm Chart Install
-NAME: minio-1603466827
-LAST DEPLOYED: Fri Oct 23 17:27:09 2020
-NAMESPACE: minio
-STATUS: deployed
-REVISION: 1
-TEST SUITE: None
-NOTES:
-Minio can be accessed via port 9000 on the following DNS name from within your cluster:
-minio-1603466827.minio.svc.cluster.local
-
-To access Minio from localhost, run the below commands:
-
-  1. export POD_NAME=$(kubectl get pods --namespace minio -l "release=minio-1603466827" -o jsonpath="{.items[0].metadata.name}")
-
-  2. kubectl port-forward $POD_NAME 9000 --namespace minio
-
-Read more about port forwarding here: http://kubernetes.io/docs/user-guide/kubectl/kubectl_port-forward/
-
-You can now access Minio server on http://localhost:9000. Follow the below steps to connect to Minio server with mc client:
-
-  1. Download the Minio mc client - https://docs.minio.io/docs/minio-client-quickstart-guide
-
-  2. mc config host add minio-1603466827-local http://localhost:9000 4CPXpYgppzlHhQ3g7r_ZBo4pXxk6jnpw aCOqg-N5gZawYWe0HjM4rSTdVHhKZNMy S3v4
-
-  3. mc ls minio-1603466827-local
-
-Alternately, you can use your browser or the Minio SDK to access the server - https://docs.minio.io/categories/17
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Gallery from "react-photo-gallery"
 import {useCallback, useState} from "react";
 import Carousel, { Modal, ModalGateway } from "react-images";
 
 export default function Home({images}) {
+    const headerImage = images.find(i => i.name === "cropped-frontrull2tett.jpg")
     const [currentImage, setCurrentImage] = useState(0);
     const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
@@ -19,21 +19,21 @@ export default function Home({images}) {
     };
 
     return (
-        <div className={styles.container}>
+        <div style={{background: '#ffffff', marginTop: '3em'}} className={"container"}>
             <Head>
                 <title>Monica Larsen</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    MONICA LARSEN
-                </h1>
-                <h2>photographer</h2>
-                <Gallery
-                    photos={images}
-                    onClick={openLightbox}
-                />
+            <main>
+                <div className={"col"}>
+                    <img className="img-fluid"
+                         src={headerImage.src}
+                         alt="Monica Larsen, Photographer"/>
+                    <Gallery
+                        photos={images}
+                        onClick={openLightbox}
+                    /></div>
                 <ModalGateway>
                     {viewerIsOpen ? (
                         <Modal onClose={closeLightbox}>
@@ -50,7 +50,7 @@ export default function Home({images}) {
                 </ModalGateway>
             </main>
 
-            <footer className={styles.footer}>
+            <footer className={"my-5 pt-5 text-muted text-center text-small"}>
                 <p>
                     About me
                     I regularly shoot images in the industrial sectors and for corporate business. I fell in love with visual art craft after working in an advertising agency. After living in London for almost 3 years working as a Photographer´s assistant I opened up my own studio in 2007 and have since then been shooting mostly advertising and editorial images for a wide range of clients, on location and in studio. I always aim to shoot images that connect emotionally with audiences. When it comes to post-production I do a lot of my own retouching, but I also work with some excellent retouching companies in Norway and abroad. I tailor the image exactly to what the client want to communicate so please feel free to contact me.   Tel +47-99229821
